0a1c42d234b41018575d7c3de8b96515406af6af
lenidar <55443074+lenidar@users.noreply.github.com>
2023-09-21 12:21:49 +0800

    AI is working, still some bugs though

--- 2Y_2324_OOP_MP1_TicTacToe/Program.cs
+++ 2Y_2324_OOP_MP1_TicTacToe/Program.cs
@@ -45,7 +45,7 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
             int[] moveCoord = { };
             List<int> boardState = new List<int>();
 
-            for(int x = 1; x <= 9; x++)
+            for(int x = 0; x < 9; x++)
                 boardState.Add(x);
 
             while(true)
@@ -54,7 +54,7 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
                 if(playerTurn) 
                     moveCoord = PlayerMove();
                 else
-                    moveCoord = ComputerMove(boardState);
+                    moveCoord = ComputerMove(boardState, board, playerFirst);
 
                 if (ValidateMove(board, moveCoord))
                 {
@@ -62,7 +62,8 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
                     boardState.Remove(CalculateForBoardState(moveCoord));
                     if (CheckVictory(board, playerTurn))
                     {
-                        Console.WriteLine("Congratulations!!!!");
+                        DisplayBoard(board);
+                        Console.WriteLine($"Congratulations!!!! {playerTurn}");
                         Console.ReadKey();
                         break;
                     }
@@ -71,6 +72,11 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
                 else if (playerTurn)
                 {
                     Console.WriteLine("\nYou performed an invalid move! Press any key to try again.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine($"\nComputer tried to move to {moveCoord[0]} - {moveCoord[1]}");
                     Console.ReadKey();
                 }
                 DisplayBoard(board);
@@ -96,8 +102,8 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
             {
                 
                 Console.WriteLine("Please enter valid coordinates using the following format x-y." +
-                    "\nx is the column number (0-2)" +
-                    "\ny is the row number (0-2)" +
+                    "\nx is the row number (0-2)" +
+                    "\ny is the column number (0-2)" +
                     "\n");
                 uInput = Console.ReadLine();
                 uInputs = uInput.Split('-');
@@ -118,12 +124,157 @@ namespace _2Y_2324_OOP_MP1_TicTacToe
             return moveCoords;
         }
 
-        static int[] ComputerMove(List<int> boardState)
+        static int[] ComputerMove(List<int> boardState, char[,] board, bool playerFirst)
         {
             int[] moveCoords = new int[] { 0, 0 };
-            int boardMove = boardState[_rnd.Next(boardState.Count)];
+            int boardMove = -1;
+
+            if (!playerFirst && boardState.Count == 9) // computer first and its the first move
+                boardMove = 4; // take center
+            else if (boardState.Count == 8) // computer moves second and its the first move
+            {
+                // second move
+                if (!boardState.Contains(4)) // if center is taken
+                    boardMove = (_rnd.Next(0, 4) * 2) + 1;
+                else
+                    boardMove = 4; // control center
+            }
+            else
+            {
+
+                Console.WriteLine("Checking Defense...");
+                // row check
+                for (int x = 0; x < board.GetLength(0); x++)
+                {
+                    if (board[x, 0] == ' ' || board[x, 1] == ' ' || board[x, 2] == ' ')// if all 3 spaces are taken, its not worth checking
+                    {
+                        if (board[x, 0] == 'O' && board[x, 1] == 'O' && board[x, 2] == ' ')
+                            boardMove = 2 + (2 * x);
+                        else if (board[x, 0] == 'O' && board[x, 1] == ' ' && board[x, 2] == 'O')
+                            boardMove = 1 + (2 * x);
+                        else if (board[x, 0] == ' ' && board[x, 1] == 'O' && board[x, 2] == 'O')
+                            boardMove = 0 + (2 * x);
+                    }
+
+                    if (boardMove > 0) // if a move has been selected, break the loop
+                        break;
+                }
+                // column check
+                if (boardMove == -1) // no move selected
+                {
+                    for (int x = 0; x < board.GetLength(1); x++)
+                    {
+                        if (board[0, x] == ' ' || board[1, x] == ' ' || board[2, x] == ' ')// if all 3 spaces are taken, its not worth checking
+                        {
+                            if (board[0, x] == 'O' && board[1, x] == 'O' && board[2, x] == ' ')
+                                boardMove = 0 + x;
+                            else if (board[0, x] == 'O' && board[1, x] == ' ' && board[2, x] == 'O')
+                                boardMove = 3 + x;
+                            else if (board[0, x] == ' ' && board[1, x] == 'O' && board[2, x] == 'O')
+                                boardMove = 6 + x;
+                        }
+
+                        if (boardMove > 0) // if a move has been selected, break the loop
+                            break;
+                    }
+                }
+
+                // row check
+                if (boardMove == -1)
+                {
+                    if (board[0, 0] == ' ' || board[1, 1] == ' ' || board[2, 2] == ' ')
+                    {
+                        if (board[0, 0] == 'O' && board[1, 1] == 'O' && board[2, 2] == ' ')
+                            boardMove = 8;
+                        else if (board[0, 0] == 'O' && board[1, 1] == ' ' && board[2, 2] == 'O')
+                            boardMove = 4;
+                        else if (board[0, 0] == ' ' && board[1, 1] == 'O' && board[2, 2] == 'O')
+                            boardMove = 0;
+                    }
+
+                    if (board[0, 2] == ' ' || board[1, 1] == ' ' || board[2, 0] == ' ')
+                    {
+                        if (board[0, 2] == 'O' && board[1, 1] == 'O' && board[2, 0] == ' ')
+                            boardMove = 6;
+                        else if (board[0, 2] == 'O' && board[1, 1] == ' ' && board[2, 0] == 'O')
+                            boardMove = 4;
+                        else if (board[0, 2] == ' ' && board[1, 1] == 'O' && board[2, 0] == 'O')
+                            boardMove = 2;
+                    }
+                }
+
+                Console.WriteLine("Done Checking Defense...");
+
+
+                Console.WriteLine("Checking Offense...");
+                // row check
+                for (int x = 0; x < board.GetLength(0); x++)
+                {
+                    if (board[x, 0] == ' ' || board[x, 1] == ' ' || board[x, 2] == ' ')// if all 3 spaces are taken, its not worth checking
+                    {
+                        if (board[x, 0] == 'X' && board[x, 1] == 'X' && board[x, 2] == ' ')
+                            boardMove = 2 + (2 * x);
+                        else if (board[x, 0] == 'X' && board[x, 1] == ' ' && board[x, 2] == 'X')
+                            boardMove = 1 + (2 * x);
+                        else if (board[x, 0] == ' ' && board[x, 1] == 'X' && board[x, 2] == 'X')
+                            boardMove = 0 + (2 * x);
+                    }
+
+                    if (boardMove > 0) // if a move has been selected, break the loop
+                        break;
+                }
+                // column check
+
+                for (int x = 0; x < board.GetLength(1); x++)
+                {
+                    if (board[0, x] == ' ' || board[1, x] == ' ' || board[2, x] == ' ')// if all 3 spaces are taken, its not worth checking
+                    {
+                        if (board[0, x] == 'X' && board[1, x] == 'X' && board[2, x] == ' ')
+                            boardMove = 0 + x;
+                        else if (board[0, x] == 'X' && board[1, x] == ' ' && board[2, x] == 'X')
+                            boardMove = 3 + x;
+                        else if (board[0, x] == ' ' && board[1, x] == 'X' && board[2, x] == 'X')
+                            boardMove = 6 + x;
+                    }
+
+                    if (boardMove > 0) // if a move has been selected, break the loop
+                        break;
+                }
+
+
+                // row check
+                if (board[0, 0] == ' ' || board[1, 1] == ' ' || board[2, 2] == ' ')
+                {
+                    if (board[0, 0] == 'X' && board[1, 1] == 'X' && board[2, 2] == ' ')
+                        boardMove = 8;
+                    else if (board[0, 0] == 'X' && board[1, 1] == ' ' && board[2, 2] == 'X')
+                        boardMove = 4;
+                    else if (board[0, 0] == ' ' && board[1, 1] == 'X' && board[2, 2] == 'X')
+                        boardMove = 0;
+                }
+
+                if (board[0, 2] == ' ' || board[1, 1] == ' ' || board[2, 0] == ' ')
+                {
+                    if (board[0, 2] == 'X' && board[1, 1] == 'X' && board[2, 0] == ' ')
+                        boardMove = 6;
+                    else if (board[0, 2] == 'X' && board[1, 1] == ' ' && board[2, 0] == 'X')
+                        boardMove = 4;
+                    else if (board[0, 2] == ' ' && board[1, 1] == 'X' && board[2, 0] == 'X')
+                        boardMove = 2;
+                }
+                
+
+                Console.WriteLine("Done Checking offense...");
+
+
+                if (boardMove == -1)
+                {
+                    boardMove = boardState[_rnd.Next(boardState.Count)];
+                }
+            }
+
             moveCoords[1] = boardMove % 3;
-            moveCoords[0] = (boardMove - moveCoords[1]) / 3;            
+            moveCoords[0] = (boardMove - moveCoords[1]) / 3;
 
             return moveCoords;
         }
